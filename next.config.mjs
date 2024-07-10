eb560149340cbcc94b9dd33c13cd5385a7595c82
cherr0406 <82948891+cherr0406@users.noreply.github.com>
2024-07-11 00:13:44 +0900

    feat: enable to write in Markdown and MDX

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = { output: "export" };
+import withMDX from "@next/mdx";
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Configure `pageExtensions` to include MDX files
+  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+  // Optionally, add any other Next.js config below
+  output: "export",
+};
+
+// Create a function that wraps nextConfig with withMDX
+const withMDXConfig = withMDX({
+  // Add any MDX-specific options here
+  extension: /\.mdx?$/,
+});
+
+// Export the wrapped configuration
+export default withMDXConfig(nextConfig);
