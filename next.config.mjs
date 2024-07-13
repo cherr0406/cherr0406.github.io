6d854651031d4103b026ea16bff5243a457ed6ee
cherr0406 <82948891+cherr0406@users.noreply.github.com>
2024-07-14 00:46:04 +0900

    fix: set env values for previewing

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 import withMDX from "@next/mdx";
+const isProd = process.env.NODE_ENV === "production";
+const isPreview = process.env.IS_PREVIEW === "true";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -6,6 +8,9 @@ const nextConfig = {
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
   // Optionally, add any other Next.js config below
   output: "export",
+  // Configure basePath and assetPrefix to support preview deployments
+  basePath: isPreview ? `/pr-preview-${process.env.PR_NUMBER}` : "",
+  assetPrefix: isPreview ? `/pr-preview-${process.env.PR_NUMBER}/` : "",
 };
 
 // Create a function that wraps nextConfig with withMDX
